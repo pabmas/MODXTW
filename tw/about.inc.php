3acf71ff715f141913028e2227b93a5da314245c
pabmas <pabmas@pabmas.net>
2012-12-30 23:23:38 +0800

    1230

---
+++ tw/about.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * About Traditional Chinese lexicon topic
+ *
+ * @language tw
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['about_msg'] = 'MODX 是一個 <a href="http://modxcms.com/" target="_blank">PHP Application Framework 及 Content Management System</a> ，基於 <a href="http://opensource.org/licenses/gpl-2.0.php">GNU Public License v2</a> 以及 (您可選擇) 任何較新的版本。';
+$_lang['about_title'] = '關於 MODX';
+$_lang['credits'] = '銘謝';
+$_lang['credits_database'] = 'MODX is powered by <a href="http://www.mysql.com/" target="_blank">MySQL</a> or <a href="http://www.microsoft.com/sqlserver/" target="_blank">Microsoft SQL Server</a>';
+$_lang['credits_php'] = 'MODX is powered by  <a href="http://www.php.net" target="_blank">PHP</a>。';
+$_lang['credits_sencha'] = 'MODX 在預設的管理介面使用了 <a href="http://www.sencha.com/" target="_blank">Sencha (ExtJS)</a> 。';
+$_lang['credits_smarty'] = 'MODX 在預設的管理介面使用了 <a href="http://www.smarty.net/" target="_blank">Smarty</a> 。';
+$_lang['credits_xpdo'] = 'MODX 使用 <a href="http://www.xpdo.org/" target="_blank">xPDO</a> 物件關聯對映.';
+$_lang['help_msg'] = '<p>您可以在 <a href="http://forums.modx.com/" target="_blank">MODX 官方論壇</a> 取得社群的支援。 我們正致力於完善 <a href="http://rtfm.modx.com/display/revolution20/Home" target="_blank">MODX Revolution 文件庫</a>.</p><p>回報bugs，請使用 <a href="http://bugs.modx.com/projects/revo" target="_blank">MODX\'s Issue Tracker</a>。</p>';
+$_lang['help_title'] = '取得協助';
