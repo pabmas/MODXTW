1447d8c0d49fa3699d3b080abe3adf896e0708d8
pabmas <pabmas@pabmas.net>
2012-12-31 02:58:48 +0800

    dashboard

---
+++ tw/dashboard.inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Welcome Page Traditional Chinese lexicon topic
+ *
+ * @language tw
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['active_users_none'] = '目前沒有使用者上線。';
+$_lang['add_user'] = '新增使用者';
+$_lang['create_resource'] = '新增 Resource';
+$_lang['dashboard'] = 'Dashboard';
+$_lang['documentation'] = '文件庫';
+$_lang['file_bug'] = '回報bug';
+$_lang['forums'] = '論壇';
+$_lang['help'] = 'Help';
+$_lang['modx_news'] = 'MODX 最新消息';
+$_lang['onlineusers_action'] = 'Action';
+$_lang['onlineusers_actionid'] = 'Action-ID';
+$_lang['onlineusers_ipaddress'] = '使用者的IP';
+$_lang['onlineusers_lasthit'] = 'Last hit';
+$_lang['onlineusers_message'] = '過去20分鐘使用者列表 (目前時間為 [[+curtime]])';
+$_lang['onlineusers_title'] = '目前在線上的使用者';
+$_lang['onlineusers_user'] = '使用者';
+$_lang['onlineusers_userid'] = '使用者的ID';
+$_lang['quicklinks'] = '快速連結';
+$_lang['security_notices'] = '安全性提示';
+$_lang['view_tree'] = '檢視樹狀結構';
+$_lang['welcome_back'] = '歡迎回來， [[+name]]。';
+$_lang['welcome_messages'] = '您有 <strong>%d</strong> 則訊息，其中有 <strong>%s</strong> 則尚未被讀取。';
+$_lang['welcome_title'] = '歡迎使用 MODX 內容管理平台';
+$_lang['yourinfo_message'] = '此一區域將顯示一些您的訊息：';
+$_lang['yourinfo_previous_login'] = '您上次登入時間：';
+$_lang['yourinfo_title'] = '您的訊息';
+$_lang['yourinfo_total_logins'] = '總登入次數：';
+$_lang['yourinfo_username'] = '您正以此帳號登入：';
